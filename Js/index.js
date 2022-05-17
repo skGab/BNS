eca3619e1d6e7abff6d118c7a13ec656a02c4576
zdeep10 <gabriel.wti02@gmail.com>
2022-05-17 09:33:44 -0300

    Nav Contact

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,17 @@
-const navContainer = document.querySelector("home__navContainer");
+const contact = document.querySelector(".contactContainer");
+const navContainer = document.querySelector(".home__navContainer");
 
+// Open nav contact
+contact.addEventListener("click", function () {
+  navContainer.classList.add("display");
 
+  contact.style.display = "none";
+});
+
+// Close nav
+const closeButton = document.querySelector(".closeIcon");
+
+closeButton.addEventListener("click", function () {
+  navContainer.classList.remove("display");
+  contact.style.display = "block";
+});
